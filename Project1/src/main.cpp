cc4ff7a32cc2a12e1c05beb51206f760eacef771
lapie <lapie@x123>
2014-03-07 15:51:51 +0100

    Vincent

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -7,11 +7,26 @@
 
 #include <iostream>
 #include <gtest/gtest.h>
-#include "Myclass.h"
+#include "Group.hpp"
+#include "Person.hpp"
+#include <vector>
 
 using namespace std;
 int main(int argc, char **argv)
 {
+	vector<Person> myvectPerson;
+	Group GroupOne;
+	int nbPerson;
+	cout << "Nb de person" << endl;
+	cin >> nbPerson;
+	GroupOne.setNumberOfPerson(nbPerson);
+	string name, phoneNumber;
+	for(i=1;i<=GroupOne.getNumberOfPerson();i++)
+	{
+		Person P1;
+
+	}
+
 	::testing::InitGoogleTest(&argc, argv);
 
 
